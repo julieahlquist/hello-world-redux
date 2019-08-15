1f2f7b18463c66dbf28594aab8aeb0f3b95d0b9c
Julie Ahlquist <julieahlquist@hotmail.com>
2019-08-15 18:00:28 +0200

    adds input for user to write own greeting, by use of onBlur event

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Container, Header, Button } from 'semantic-ui-react';
+import { Container, Header, Button, Input } from 'semantic-ui-react';
 
 const App = (props) => {
   return (
     <>
       <Container>
        <Header as='h1'>{props.state.greeting}</Header>
-      <Button
-        primary
-        onClick={() => props.dispatch({ type: 'CHANGE_GREETING',
-  greeting: 'Hello Mars' })}
+        <Input
+          placeholder='How would you like to be greeted?'
+          onBlur={(event) => props.dispatch({ type: 'PROPOSE_GREETING',
+        greeting: event.target.value })}
+        />
+        <Button
+          primary
+          onClick={() => props.dispatch({ type: 'CHANGE_GREETING' })}
         >
           Change greeting
         </Button>
