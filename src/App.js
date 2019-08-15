8584bc1dcf8a22b34d29088cde7b8d1df7d1aa48
Julie Ahlquist <julieahlquist@hotmail.com>
2019-08-15 17:50:41 +0200

    changes greeting message by if/else statement and adds button to change

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import {Container, Header} from 'semantic-ui-react';
+import { Container, Header, Button } from 'semantic-ui-react';
 
 const App = (props) => {
   return (
     <>
       <Container>
        <Header as='h1'>{props.state.greeting}</Header>
+      <Button
+        primary
+        onClick={() => props.dispatch({ type: 'CHANGE_GREETING',
+  greeting: 'Hello Mars' })}
+        >
+          Change greeting
+        </Button>
       </Container>
     </>
   );
