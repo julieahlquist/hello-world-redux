0f5753284d3c6289bc512fe12664e1a15a68d1bd
Julie Ahlquist <julieahlquist@hotmail.com>
2019-08-17 12:55:55 +0200

    adds menu as header

--- src/Greeting.jsx
+++ src/Greeting.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Header } from 'semantic-ui-react';
+import { Menu, Header } from 'semantic-ui-react';
 
 const Greeting = (props) => {
   return (
-      <>
+    <>
+      <Menu>
+        <Menu.Item header>HELLO REDUX</Menu.Item>
+        <Menu.Item name='Environment'/>
+        <Menu.Item name='Medicine' />
+        <Menu.Item name='Outreach' />
+      </Menu>
       <Header as="h1">{props.greeting}</Header>
-      </>
+    </>
   )
 }
 
