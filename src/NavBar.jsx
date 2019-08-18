8ecc66b598d225fdc82dc60d3cfdf052f2a5b643
Daria Lykova <daria.lykova@gmail.com>
2019-08-18 15:12:12 +0200

    Fixes the css files/responsive design working renames menu categories for styling modifies the colors

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -10,20 +10,20 @@ export default class NavBar extends Component {
     const { activeItem } = this.state
     return (
       <>
-      <div className='background'>
-      <Header id='header' textAlign='center' as='h1'>
+      <div className='page'>
+      <Header id='header' as='h1' textAlign='centered'>
         GNOSIS
       </Header>
         <Menu secondary id='navbar'>
-           <Menu.Item className='category' name='Latest Publications' active={activeItem === 'latest news'} onClick={this.handleItemClick} />
-           <Menu.Item className='category' name='Environment' active={activeItem === 'environment'} onClick={this.handleItemClick} />
-           <Menu.Item className='category' name='Medicine' active={activeItem === 'medicine'} onClick={this.handleItemClick} />
-           <Menu.Item className='category' name='Outreach' active={activeItem === 'friends'} onClick={this.handleItemClick} />
+           <Menu.Item id='latest' name='Latest Publications' active={activeItem === 'latest news'} onClick={this.handleItemClick} />
+           <Menu.Item id='environment'name='Environment' active={activeItem === 'environment'} onClick={this.handleItemClick} />
+           <Menu.Item id='medicine' name='Medicine' active={activeItem === 'medicine'} onClick={this.handleItemClick} />
+           <Menu.Item id='outreach'name='Outreach' active={activeItem === 'friends'} onClick={this.handleItemClick} />
            <Menu.Menu position='right'>
             <Menu.Item>
-              <Input className='category' id='search' icon='search' placeholder='Search...' />
+              <Input id='search' icon='search' placeholder='Search...' />
             </Menu.Item>
-            <Menu.Item className='category' name='Log In' active={activeItem === 'login'} onClick={this.handleItemClick} />
+            <Menu.Item id='login' name='Log In' active={activeItem === 'login'} onClick={this.handleItemClick} />
           </Menu.Menu>
         </Menu>
         </div>
