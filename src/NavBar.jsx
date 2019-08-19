fcb82828b0796414f90bc70eb2b8c3f3b1240664
Julie Ahlquist <julieahlquist@hotmail.com>
2019-08-19 13:23:44 +0200

    refactors menu items and fixes font color

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -15,15 +15,15 @@ export default class NavBar extends Component {
         GNOSIS
       </Header>
         <Menu secondary id='navbar'>
-           <Menu.Item id='latest' name='Latest Publications' active={activeItem === 'latest news'} onClick={this.handleItemClick} />
-           <Menu.Item id='environment'name='Environment' active={activeItem === 'environment'} onClick={this.handleItemClick} />
-           <Menu.Item id='medicine' name='Medicine' active={activeItem === 'medicine'} onClick={this.handleItemClick} />
-           <Menu.Item id='outreach'name='Outreach' active={activeItem === 'friends'} onClick={this.handleItemClick} />
+           <Menu.Item className='link' name='Latest Publications' active={activeItem === 'latest news'} onClick={this.handleItemClick} />
+           <Menu.Item className='link' name='Environment' active={activeItem === 'environment'} onClick={this.handleItemClick} />
+           <Menu.Item className='link' name='Medicine' active={activeItem === 'medicine'} onClick={this.handleItemClick} />
+           <Menu.Item className='link' name='Outreach' active={activeItem === 'friends'} onClick={this.handleItemClick} />
            <Menu.Menu position='right'>
             <Menu.Item>
-              <Input id='search' icon='search' placeholder='Search...' />
+              <Input className='link' icon='search' placeholder='Search...' />
             </Menu.Item>
-            <Menu.Item id='login' name='Log In' active={activeItem === 'login'} onClick={this.handleItemClick} />
+            <Menu.Item className='link' name='Log In' active={activeItem === 'login'} onClick={this.handleItemClick} />
           </Menu.Menu>
         </Menu>
         </div>
